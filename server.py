879515ce3e7bd346c66735151209e49d8db2359c
B3n3m0th <b3n3m0th@gmail.com>
2021-08-02 16:10:00 +0300

    Third version

--- server.py
+++ server.py
@@ -3,8 +3,8 @@ import binascii
 import requests
 import time
 timeout = 10000 
-host = '192.168.3.253' #your ip address
-port = 8888 #listening port
+host = '192.168.3.253'              #your ip address
+port = 8888                         #listening port
 addr = (host, port)
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("",port))
@@ -16,13 +16,13 @@ while True:
 	    print('Time is out. {0} seconds have passed'.format(timeout))
     received = d[0]
     addr = d[1]
-    data_bytes = binascii.hexlify(received) #convert UDP packets to HEX
-    data_string = str(data_bytes) # Parsing some data from UDP packet
-    module_id = data_string[27] # Parsing some data from UDP packet
-    spo2_hex = str(data_string[34]+data_string[35]) # Parsing some data from UDP packet
-    pulse_hex = str(data_string[46]+data_string[47]) # Parsing some data from UDP packet
-    spo2_int = int(spo2_hex, 16) # Parsing some data from UDP packet
-    pulse_int = int(pulse_hex, 16) # Parsing some data from UDP packet
+    data_bytes = binascii.hexlify(received)                     #convert UDP packets to HEX
+    data_string = str(data_bytes)                               # Parsing some data from UDP packet
+    module_id = data_string[27]                                 # Parsing some data from UDP packet
+    spo2_hex = str(data_string[34]+data_string[35])             # Parsing some data from UDP packet
+    pulse_hex = str(data_string[46]+data_string[47])            # Parsing some data from UDP packet
+    spo2_int = int(spo2_hex, 16)                                # Parsing some data from UDP packet
+    pulse_int = int(pulse_hex, 16)                              # Parsing some data from UDP packet
     if module_id == '6':
         print(data_bytes)
         print(spo2_int)
@@ -32,6 +32,6 @@ while True:
         "spo2_int" : f'{spo2_int}'
         }
         headers = {'Content-type': 'application/json'}
-        r = requests.post(f'http://{host}:5000/post', headers = headers, json = body) #POST request to flask server on port 5000
+        r = requests.post(f'http://{host}:5000/post', headers = headers, json = body)                       #POST request to flask server on port 5000
         print(r)
 server.close()
