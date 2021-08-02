7b6d4f8e6dd988838c2bd519b6fa78a56a346c09
B3n3m0th <b3n3m0th@gmail.com>
2021-08-02 16:06:28 +0300

    Second version

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ import socket
 import binascii
 import requests
 import time
-timeout = 10000
-host = '192.168.3.253'
-port = 8888
+timeout = 10000 
+host = '192.168.3.253' #your ip address
+port = 8888 #listening port
 addr = (host, port)
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind(("",port))
@@ -16,13 +16,13 @@ while True:
 	    print('Time is out. {0} seconds have passed'.format(timeout))
     received = d[0]
     addr = d[1]
-    data_bytes = binascii.hexlify(received)
-    data_string = str(data_bytes)
-    module_id = data_string[27]
-    spo2_hex = str(data_string[34]+data_string[35])
-    pulse_hex = str(data_string[46]+data_string[47])
-    spo2_int = int(spo2_hex, 16)
-    pulse_int = int(pulse_hex, 16)
+    data_bytes = binascii.hexlify(received) #convert UDP packets to HEX
+    data_string = str(data_bytes) # Parsing some data from UDP packet
+    module_id = data_string[27] # Parsing some data from UDP packet
+    spo2_hex = str(data_string[34]+data_string[35]) # Parsing some data from UDP packet
+    pulse_hex = str(data_string[46]+data_string[47]) # Parsing some data from UDP packet
+    spo2_int = int(spo2_hex, 16) # Parsing some data from UDP packet
+    pulse_int = int(pulse_hex, 16) # Parsing some data from UDP packet
     if module_id == '6':
         print(data_bytes)
         print(spo2_int)
@@ -32,6 +32,6 @@ while True:
         "spo2_int" : f'{spo2_int}'
         }
         headers = {'Content-type': 'application/json'}
-        r = requests.post(f'http://{host}:5000/post', headers = headers, json = body)
+        r = requests.post(f'http://{host}:5000/post', headers = headers, json = body) #POST request to flask server on port 5000
         print(r)
 server.close()
